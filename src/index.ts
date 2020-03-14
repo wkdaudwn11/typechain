54b958948091c85692e7d38892e388d8c081a90c
JangMyungJoo <wkdaudwn1028@gmail.com>
2020-03-14 09:31:04 +0900

    #5 Classes on Typescript part One

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,33 @@
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+// interface Human {
+//   name: string;
+//   age: number;
+//   gender: string;
+// }
+
+// const person = {
+//   name: "명주",
+//   age: 29,
+//   gender: "남자"
+// };
+
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: "명주",
-  age: 29,
-  gender: "남자"
-};
+const lynn = new Human("Lynn", 18, "female");
 
 const getInfo = (person: Human): string => {
   return `이름: ${person.name}, 나이: ${person.age}, 성별: ${person.gender}`;
 };
 
-const val: string = getInfo(person);
+const val: string = getInfo(lynn);
 console.log(val);
 
 export {};
